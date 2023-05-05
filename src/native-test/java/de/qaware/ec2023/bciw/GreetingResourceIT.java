52f1676e8b1113ced791f593936f7371afd8d4d2
Dominik Haas <dominik.haas@qaware.de>
2023-05-05 13:45:12 +0200

    Create initial quarkus project

---
+++ src/native-test/java/de/qaware/ec2023/bciw/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package de.qaware.ec2023.bciw;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
