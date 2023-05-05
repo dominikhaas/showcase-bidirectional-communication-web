e659d2ab2ae70a076e38418e03acd325c63b6410
Dominik Haas <dominik.haas@qaware.de>
2023-05-05 14:13:34 +0200

    Remove starter template stuff

--- src/native-test/java/de/qaware/ec2023/bciw/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package de.qaware.ec2023.bciw;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
