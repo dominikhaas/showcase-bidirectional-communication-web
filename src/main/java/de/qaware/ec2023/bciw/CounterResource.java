65d2ff23ba4dd7a772ec603eedda25240078abf5
Dominik Haas <dominik.haas@qaware.de>
2023-05-09 10:53:52 +0200

    Add sse implementation (still not working)

--- src/main/java/de/qaware/ec2023/bciw/CounterResource.java
+++ src/main/java/de/qaware/ec2023/bciw/CounterResource.java
@@ -4,27 +4,86 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.sse.OutboundSseEvent;
+import jakarta.ws.rs.sse.Sse;
+import jakarta.ws.rs.sse.SseBroadcaster;
+import jakarta.ws.rs.sse.SseEventSink;
+
+import java.util.UUID;
 
 @Path("/counter")
 @Produces(MediaType.APPLICATION_JSON)
 public class CounterResource {
     private int counter = 0;
 
+
+    /*
+     * SSE implementation
+     */
+    private OutboundSseEvent.Builder eventBuilder;
+    private SseBroadcaster sseBroadcaster;
+
+
+    public CounterResource(@Context final Sse sse) {
+        this.eventBuilder = sse.newEventBuilder();
+        this.sseBroadcaster = sse.newBroadcaster();
+    }
+
     @GET
     public CounterValue currentCounter() {
         return new CounterValue(counter);
-    };
+    }
+
+    ;
 
     @POST
     @Path("/increment")
     public void increment() {
         counter++;
+        notifySubscriptions();
     }
 
     @POST
     @Path("/decrement")
     public void decrement() {
         counter--;
+        notifySubscriptions();
+    }
+
+
+    private void notifySubscriptions() {
+        notifySse();
+    }
+
+    /*
+     * SSE implementation
+     */
+    @GET
+    @Path("/subscribe")
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public void subscribe(@Context SseEventSink sseEventSink) throws InterruptedException {
+
+        OutboundSseEvent sseEvent = this.eventBuilder
+                .name("welcome")
+                .mediaType(MediaType.TEXT_PLAIN_TYPE)
+                .data("welcome!")
+                .build();
+
+        sseEventSink.send(sseEvent);
+    }
+
+    private void notifySse() {
+        OutboundSseEvent sseEvent = this.eventBuilder
+                .name("counter")
+                .id(UUID.randomUUID().toString())
+                .mediaType(MediaType.APPLICATION_JSON_TYPE)
+                .data(CounterValue.class, new CounterValue(counter))
+                .reconnectDelay(4000)
+                .comment("counter changed")
+                .build();
+
+        this.sseBroadcaster.broadcast(sseEvent);
     }
 }
